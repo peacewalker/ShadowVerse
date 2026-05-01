b26d0588dbc1f855b7333aa4d3cbda19d1b18a16
peacewalker <peacewalker@users.noreply.github.com>
2026-05-01 17:43:06 +0800

    Update state_type macro to use crate::state::State

--- src-tauri/src/handlers/macros.rs
+++ src-tauri/src/handlers/macros.rs
@@ -2,7 +2,7 @@
 #[macro_export]
 macro_rules! state_type {
     () => {
-        tauri::State<'_, State>
+        tauri::State<'_, crate::state::State>
     };
 }
 
@@ -10,6 +10,6 @@ macro_rules! state_type {
 #[macro_export]
 macro_rules! state_type {
     () => {
-        State
+        crate::state::State
     };
 }
